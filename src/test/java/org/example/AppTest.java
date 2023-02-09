f8046cb09056f459c51e859297469a8d0ac98a7a
vanessapptim <89786732+vanessaptim@users.noreply.github.com>
2023-02-09 16:01:37 -0300

    first commit

--- src/test/java/org/example/AppTest.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class AppTest 
-    extends BaseTest
-{
-    public String nameField = "com.androidsample.generalstore:id/nameField";
-    public String radioButtonFemale = "//android.widget.RadioButton[@text='Female']";
-
-}
